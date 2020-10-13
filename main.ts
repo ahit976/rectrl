abdb17113a63871567867d332c297f47066e567d
ahit976 <55730608+ahit976@users.noreply.github.com>
2020-10-13 20:21:20 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,28 @@
+gamePad.onEvent(GamerBitPin.P15, GamerBitEvent.Up, function () {
+    radio.sendNumber(0)
+})
+gamePad.onEvent(GamerBitPin.P14, GamerBitEvent.Up, function () {
+    radio.sendNumber(0)
+})
+gamePad.onEvent(GamerBitPin.P16, GamerBitEvent.Up, function () {
+    radio.sendNumber(0)
+})
+gamePad.onEvent(GamerBitPin.P13, GamerBitEvent.Up, function () {
+    radio.sendNumber(0)
+})
+gamePad.onEvent(GamerBitPin.P13, GamerBitEvent.Down, function () {
+    radio.sendNumber(2)
+})
+gamePad.onEvent(GamerBitPin.P15, GamerBitEvent.Down, function () {
+    radio.sendNumber(1)
+})
+gamePad.onEvent(GamerBitPin.P14, GamerBitEvent.Down, function () {
+    radio.sendNumber(4)
+})
+gamePad.onEvent(GamerBitPin.P16, GamerBitEvent.Down, function () {
+    radio.sendNumber(3)
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
