0f6a755a0bc5f747f63cc45a6b4a36cb41931a26
ahit976 <55730608+ahit976@users.noreply.github.com>
2020-10-14 19:27:47 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,16 +11,32 @@ gamePad.onEvent(GamerBitPin.P13, GamerBitEvent.Up, function () {
     radio.sendNumber(0)
 })
 gamePad.onEvent(GamerBitPin.P13, GamerBitEvent.Down, function () {
-    radio.sendNumber(2)
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(6)
+    } else {
+        radio.sendNumber(2)
+    }
 })
 gamePad.onEvent(GamerBitPin.P15, GamerBitEvent.Down, function () {
-    radio.sendNumber(1)
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(5)
+    } else {
+        radio.sendNumber(1)
+    }
 })
 gamePad.onEvent(GamerBitPin.P14, GamerBitEvent.Down, function () {
-    radio.sendNumber(4)
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(8)
+    } else {
+        radio.sendNumber(4)
+    }
 })
 gamePad.onEvent(GamerBitPin.P16, GamerBitEvent.Down, function () {
-    radio.sendNumber(3)
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(7)
+    } else {
+        radio.sendNumber(3)
+    }
 })
 radio.setGroup(1)
 basic.forever(function () {
